504e95dd5ff351fde63920df69fca6c42c4c3d88
MahirSUthar <mahirsuthar28@gmail.com>
2026-02-16 17:38:01 +0530

    C

---
+++ C/Data type and variables/Add,sub,multiply(float).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+	float A=10.10;
+	float B=1.19;
+	float sum = A+B;
+	float sub =A-B;
+	float multiply = A*B;
+		printf("\nAddition = ,%.2f",sum);
+		printf("\nSubstraction = ,%.2f",sub);
+		printf("\nMultiplication = ,%.2f",multiply);
+}
