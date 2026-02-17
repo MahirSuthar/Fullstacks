898345f8aa0fd1b1dca041400492e49f78feaad7
MahirSUthar <mahirsuthar28@gmail.com>
2026-02-17 22:43:41 +0530

    03_clc.exe

---
+++ C/Data type and variables/03_clc.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+{
+ 	
+	 
+	  float a,b;
+ 	
+ 	printf("Enter the value of A = ");
+ 	scanf("%f",&a);
+ 	printf("A : %2.f",a);
+ 	
+ 	printf("Enter the value of B = ");
+ 	scanf ("%f",&b);
+ 	printf("B : %2.f",b);
+ 	 
+ 	 printf("\nsum = %.2f",a+b);
+ 	 printf("\nsub= %.2f",a-b);
+ 	 printf("\nmultiply = %.2f",a*b);
+ 	 printf("\ndiv = %.2f",a/b);
+ }
