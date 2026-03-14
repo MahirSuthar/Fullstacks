cf29f314d36a3f2d5877e9757d1440016c50e701
MahirSuthar <mahirsuthar28@gmail.com>
2026-03-14 15:59:35 +0530

    Fullstacks

---
+++ langg . c++/Inheritance/01_hybrid.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using nae space std;
+
+class A 
+{
+	public ()
+	{
+		A ()
+		{
+			cout<<"This is A";
+		}
+	}
+};
+class B
+{
+		public ()
+		{
+			B ()
+			{
+				cout<<"This is  B";
+			}
+		}
+};
+int mainn ()
+{
+	return 0;
+}
